78ae14d65a6a2910083887bbaeae75a8d34e382b
vbukoev <vbukoev@gmail.com>
2023-10-20 19:49:52 +0300

    adding the project files

---
+++ SchoolManagementSystem/SchoolManagementSystem/Admin/AddClass.aspx.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SchoolManagementSystem.Models;
+using static SchoolManagementSystem.Models.CommonFn;
+
+namespace SchoolManagementSystem.Admin
+{
+    public partial class AddClass : System.Web.UI.Page
+    {
+        CommonFnx fn = new CommonFnx();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetClass();
+            }
+        }
+
+        private void GetClass() //attaching the GetClass Method to the DB
+        {
+            DataTable dt = fn.Fetch("Select Row_NUMBER() over(Order by (Select 1)) as [Sr.No], ClassId, ClassName from Class");
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataTable dt = fn.Fetch("Select * from Class where ClassName = '" + txtClass.Text.Trim() + "' ");
+                if (dt.Rows.Count == 0)
+                {
+                    string query = "Insert into Class values('" + txtClass.Text.Trim() + "')";
+                    fn.Query(query);
+                    lblMsg.Text = "Inserted Successfully!";
+                    lblMsg.CssClass = "alert alert-success";
+                    txtClass.Text = string.Empty;
+                    GetClass();
+                }
+                else
+                {
+                    lblMsg.Text = "Entered Class already exists";
+                    lblMsg.CssClass = "alert alert-danger";
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            GetClass();
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            GetClass();
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+        }
+
+        protected void GridView_1RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {
+                GridViewRow row = GridView1.Rows[e.RowIndex];
+                int cId = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+                string ClassName = (row.FindControl("txtClassEdit") as TextBox).Text;
+                fn.Query("Update Class set ClassName = '"+ClassName+"' where ClassId = '"+ cId+ "'");
+                lblMsg.Text = "Inserted Successfully!";
+                lblMsg.CssClass = "alert alert-success";
+                GridView1.EditIndex = -1;
+                GetClass();
+            }
+            catch (Exception exception)
+            {
+                Response.Write("<script>alert('" + exception.Message + "');</script>");
+            }
+        }
+    }
+}
---
+++ SchoolManagementSystem/SchoolManagementSystem/Admin/AdminHome.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/AdminMst.Master" AutoEventWireup="true" CodeBehind="AdminHome.aspx.cs" Inherits="SchoolManagementSystem.Admin.AdminHome" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    
+    <div style="background-image: url('../Image/background-image.jpg'); width: 100%; height: 900px; background-repeat: no-repeat; background-size: cover; background-attachment: fixed">
+        <div class="container p-md-4 p-sm-4">
+            <div>
+                <asp:Label ID="lblMsg" runat="server"></asp:Label>
+            </div>
+            <h2 class="text-center">Admin Home Page</h2>
+        </div>
+    </div>
+</asp:Content>
