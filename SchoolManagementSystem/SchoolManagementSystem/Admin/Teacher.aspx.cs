c06717f8ec1299a1bbe5ea0c69d57155bc0d4e5a
vbukoev <vbukoev@gmail.com>
2023-10-21 11:47:44 +0300

    Create Teacher.aspx.cs

---
+++ SchoolManagementSystem/SchoolManagementSystem/Admin/Teacher.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SchoolManagementSystem.Admin
+{
+    public partial class Teacher : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
