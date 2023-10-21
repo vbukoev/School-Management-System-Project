4a43281f2d3bdd95c379ddc725c0dc716f1530ec
vbukoev <vbukoev@gmail.com>
2023-10-21 16:55:36 +0300

    Create TeacherSubject.aspx.cs

---
+++ SchoolManagementSystem/SchoolManagementSystem/Admin/TeacherSubject.aspx.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static SchoolManagementSystem.Models.CommonFn;
+
+namespace SchoolManagementSystem.Admin
+{
+    public partial class TeacherSubject : System.Web.UI.Page
+    {
+        CommonFnx fn = new CommonFnx();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetClass();
+                GetTeacher();
+                GetTeacherSubject();
+            }
+        }
+
+        private void GetClass()
+        {
+            DataTable dt = fn.Fetch("Select * from Class");
+            ddlClass.DataSource = dt;
+            ddlClass.DataTextField = "ClassName";
+            ddlClass.DataValueField = "ClassId";
+            ddlClass.DataBind();
+            ddlClass.Items.Insert(0, "Select Class");
+        }
+
+        private void GetTeacher()
+        {
+            DataTable dt = fn.Fetch("Select * from Teacher");
+            ddlTeacher.DataSource = dt;
+            ddlTeacher.DataTextField = "Name";
+            ddlTeacher.DataValueField = "TeacherId";
+            ddlTeacher.DataBind();
+            ddlTeacher.Items.Insert(0, "Select Teacher");
+        }
+
+        private void GetTeacherSubject()
+        {
+            DataTable dt =
+                fn.Fetch(
+                    @"Select Row_NUMBER() over(Order by(select 1)) as [Sr.No], ts.Id, ts.ClassId, c.ClassName, ts.SubjectId, s.SubjectName, ts.TeacherId, t.Name from TeacherSubject ts inner join Class c on ts.ClassId = c.ClassId inner join Subject s on ts.SubjectId = s.SubjectId inner join Teacher t on ts.TeacherId = t.TeacherId");
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void ddlClass_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string classId = ddlClass.SelectedValue;
+            DataTable dt = fn.Fetch("Select * From Subject where ClassId = '" + classId + "' "); 
+            ddlSubject.DataSource = dt;
+            ddlSubject.DataTextField = "SubjectName";               //GETSUBJECT() method is implemented here
+            ddlSubject.DataValueField = "SubjectId";
+            ddlSubject.DataBind();
+            ddlSubject.Items.Insert(0, "Select Teacher");
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string classId = ddlClass.SelectedValue;
+                string subjectId = ddlSubject.SelectedValue;
+                string teacherId = ddlTeacher.SelectedValue;
+
+                DataTable dt = fn.Fetch("Select * from TeacherSubject where ClassId = '" + classId + "' and SubjectId='" + subjectId + "' and TeacherId = '" + teacherId + "'");
+                if (dt.Rows.Count == 0)
+                {
+                    string query = "Insert into Subject values('" + classId + "','" + subjectId + "', '" + teacherId + "')";
+                    fn.Query(query);
+                    lblMsg.Text = "Inserted Successfully!";
+                    lblMsg.CssClass = "alert alert-success";
+                    ddlClass.SelectedIndex = 0;
+                    ddlSubject.SelectedIndex = 0;
+                    ddlTeacher.SelectedIndex = 0;
+                    GetTeacherSubject();
+                }
+                else
+                {
+                    lblMsg.Text = "Entered <b>Teacher Subject</b> already exists!";
+                    lblMsg.CssClass = "alert alert-danger";
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+
+        }
+
+    }
+}
