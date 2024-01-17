56eca8cabe61789c00b649f277c2046e96d3be6e
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-01-17 10:00:42 +0200

    Update ClassFees.aspx.cs

--- SchoolManagementSystem/SchoolManagementSystem/Admin/ClassFees.aspx.cs
+++ SchoolManagementSystem/SchoolManagementSystem/Admin/ClassFees.aspx.cs
@@ -15,6 +15,11 @@ namespace SchoolManagementSystem.Admin
         CommonFnx fn = new CommonFnx();
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["admin"] == null)
+            {
+                Response.Redirect("../Login.aspx");
+            }
+
             if (!IsPostBack)
             {
                 GetClass();
