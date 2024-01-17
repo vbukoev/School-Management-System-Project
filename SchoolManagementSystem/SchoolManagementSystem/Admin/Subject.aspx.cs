ca93723df6bc49767a2c21ef61f6a7feedc33944
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-01-17 19:40:09 +0200

    Update Subject.aspx.cs

--- SchoolManagementSystem/SchoolManagementSystem/Admin/Subject.aspx.cs
+++ SchoolManagementSystem/SchoolManagementSystem/Admin/Subject.aspx.cs
@@ -14,6 +14,11 @@ namespace SchoolManagementSystem.Admin
         CommonFnx fn = new CommonFnx();
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["admin"] == null)
+            {
+                Response.Redirect("../Login.aspx");
+            }
+
             if (!IsPostBack)
             {
                 GetClass();
@@ -107,9 +112,5 @@ namespace SchoolManagementSystem.Admin
 
         }
 
-        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
     }
 }
